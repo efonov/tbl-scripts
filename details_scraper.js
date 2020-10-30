98172a8a1c2641d58eba4a7497564667cee40f86
Elliot Young <43792846+elliot-wheaton@users.noreply.github.com>
2020-10-30 11:08:06 -0700

    tbl scripts init

---
+++ details_scraper.js
@@ -0,0 +1,164 @@
+const puppeteer = require("puppeteer")
+const mysql = require("mysql")
+const SqlString = require('sqlstring');
+
+const tblArr = []
+const sourceTable = "source"
+const productionTable = "production"
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "secret",
+    database: "tbl_booklist",
+})
+
+main()
+
+async function main() {
+    connection.connect(async function (err) {
+        if (err) {
+            console.error("error connecting: " + err.stack)
+            return
+        }
+        console.log(`Connected to MySQL database with ID ${connection.threadId}`)
+        connection.query(`SELECT * FROM ${sourceTable}`, async function (
+            err,
+            result,
+            fields
+        ) {
+            result.forEach((element) => {
+                tblArr.push({
+                    id: element.id,
+                    tbl_title: element.tbl_title.replace(/((, The$)|(, A$)|(,The$)|(,A$))/g, ""),
+                    tbl_author: element.tbl_author,
+                    hits: element.hits,
+                    urls: element.urls.split(/,\s/),
+                })
+            })
+        })
+    })
+
+    const browser = await puppeteer.launch({
+        headless: true,
+    })
+
+    for(i = 0; i<= tblArr.length; i++){
+        await scrapeDetails(tblArr[i])
+    }
+
+    async function scrapeDetails(element) {
+        let hits = element.hits
+        if (hits == 0) {
+            console.log(`[scrapeDetails—${element.id}] this element has no urls to scrape :(`)
+            return
+        }
+        else if (hits > 1) {
+            console.log(`[scrapeDetails—${element.id}] this element has multiple urls listed! breaking into multiple child elements.`)
+            element.urls.forEach((childURL) => {
+                let newElement = {
+                    id: (element.id * 1000) + element.urls.indexOf(childURL),
+                    tbl_title: element.tbl_title,
+                    tbl_author: element.tbl_author,
+                    hits: 1,
+                    urls: [childURL]
+                }
+                scrapeDetails(newElement)
+                return
+            })
+        }
+        else {
+            let page = await browser.newPage({})
+            let url = element.urls.pop()
+            console.log(`[scrapeDetails—${element.id}] retrieving details for '${element.tbl_title}' from ${url}`)
+            await page.goto(url).catch(()=>{return})
+            await waitForNetworkIdle(page, 500, 0)
+            let cela_details = await page.evaluate(() => {
+                let cela_details = {
+                    title: document.querySelector("div.book-details--title > h1") != null ? document.querySelector("div.book-details--title > h1").innerText : '',
+                    author: document.querySelector("p.book-details--author") != null ? document.querySelector("p.book-details--author").innerText.replace(/By /g, '') : '',
+                    genre: document.querySelector(".book-details--body > p:nth-child(1)") != null ? document.querySelector(".book-details--body > p:nth-child(1)").innerText : '',
+                    narration_type: document.querySelector(".book-details--body > p:nth-child(2)") != null ? document.querySelector(".book-details--body > p:nth-child(2)").innerText : '',
+                    summary_pt1: document.querySelector(".book-details--summary") != null ? document.querySelector(".book-details--summary").innerText.replace(/… \(Show full summary\)/g, ' ') : '',
+                    summary_pt2: document.querySelector("span.summary-remainder") != null ? document.querySelector("span.summary-remainder").innerText : '',
+                    isbn: document.querySelector(".field--name-field-isbn13") != null ? document.querySelector(".field--name-field-isbn13").innerText.replace(/ISBN[\n]/g, '') : '',
+                    publisher: document.querySelector(".field--name-field-publisher") != null ? document.querySelector(".field--name-field-publisher").innerText.replace(/Publisher[\n]/g, '') : '',
+                    copyright_date: document.querySelector(".field--name-field-copyrightdate") != null ? document.querySelector(".field--name-field-copyrightdate").innerText.replace(/Copyright Date[\n]/g, '') : '',
+                    cela_book_number: document.querySelector("div.book-details--details > div:nth-child(1) > div:nth-child(5)") != null ? document.querySelector("div.book-details--details > div:nth-child(1) > div:nth-child(5)").innerText.replace(/Book number[\n]/g, '') : '',
+                    audio_narrator: document.querySelector(".field--name-field-artifact-narrator-name") != null ? document.querySelector(".field--name-field-artifact-narrator-name").innerText.replace(/Narrator[\n]/g, '') : '',
+                    audio_duration: document.querySelector(".field--name-field-artifact-duration") != null ? document.querySelector(".field--name-field-artifact-duration").innerText.replace(/Duration[\n]/g, '') : '',
+                    audio_producer: document.querySelector(".field--name-field-artifact-audio-producer") != null ? document.querySelector(".field--name-field-artifact-audio-producer").innerText.replace(/Audio producer[\n]/g, '').replace(/, \d*.*/g, '') : '',
+                    audio_production_date: document.querySelector(".field--name-field-artifact-audio-producer") != null ? document.querySelector(".field--name-field-artifact-audio-producer").innerText.replace(/\D/g, '') : '',
+                }
+                return cela_details
+            })
+            await parseDetails(cela_details, element, url)
+            await page.close()
+            return (cela_details)
+        }
+        return
+    }
+
+    async function parseDetails(cela_details, element, url) {
+        let d = {
+            title: cela_details.title,
+            author: cela_details.author,
+            genre: cela_details.genre,
+            narration_type: cela_details.narration_type,
+            summary: `${cela_details.summary_pt1}${cela_details.summary_pt2}`,
+            isbn: cela_details.isbn,
+            publisher: cela_details.publisher,
+            copyright_date: cela_details.copyright_date,
+            cela_book_number: cela_details.cela_book_number,
+            audio_narrator: cela_details.audio_narrator,
+            audio_duration: cela_details.audio_duration,
+            audio_producer: cela_details.audio_producer,
+            audio_production_date: cela_details.audio_production_date,
+            raw_url: url,
+            wp_url: `<a href="${url}" target="_blank">${cela_details.title}</a>`,
+            google_spreadheet_url: `=HYPERLINK("${url}", "${cela_details.title}")`,
+            tbl_id: element.id
+        }
+        console.log(`[parseDetails—${element.id}] details:`, d)
+        let sqlArr = [d.title, d.author, d.genre, d.narration_type, d.summary, d.isbn, d.publisher, d.copyright_date, d.cela_book_number, d.audio_narrator, d.audio_duration, d.audio_producer, d.audio_production_date, d.raw_url, d.wp_url, d.google_spreadheet_url, d.tbl_id]
+        await postDetails(sqlArr)
+        return d
+    }
+}
+
+async function postDetails(details) {
+    var sql = `INSERT INTO ${productionTable} (title, author, genre, narration_type, summary, isbn, publisher, copyright_date, cela_book_number, audio_narrator, audio_durration, audio_producer, audio_production_date, raw_url, wp_url, google_spreadheet_url, tbl_id) VALUES (?)`;
+    connection.query(sql, [SqlString.format(details)], function (err, result) {
+      if (err) throw err;
+    })
+    console.log(`[postDetails—${details[15]}] succesfully posted details for '${details[0]}'!\n`)
+    return
+  }
+
+function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
+    page.on('request', onRequestStarted)
+    page.on('requestfinished', onRequestFinished)
+    page.on('requestfailed', onRequestFinished)
+    let inflight = 0
+    let fulfill
+    let promise = new Promise(x => fulfill = x)
+    let timeoutId = setTimeout(onTimeoutDone, timeout)
+    return promise
+    function onTimeoutDone() {
+        page.removeListener('request', onRequestStarted)
+        page.removeListener('requestfinished', onRequestFinished)
+        page.removeListener('requestfailed', onRequestFinished)
+        fulfill()
+    }
+    function onRequestStarted() {
+        ++inflight
+        if (inflight > maxInflightRequests)
+            clearTimeout(timeoutId)
+    }
+    function onRequestFinished() {
+        if (inflight === 0)
+            return
+        --inflight
+        if (inflight === maxInflightRequests)
+            timeoutId = setTimeout(onTimeoutDone, timeout)
+    }
+}
---
+++ url_scraper.js
@@ -0,0 +1,138 @@
+const puppeteer = require("puppeteer");
+const mysql = require("mysql");
+const keyword_extractor = require("keyword-extractor");
+
+const maxTabs = 4;
+var count = 0;
+var tabs = 0;
+
+main();
+
+async function main() {
+  const tblArr = [];
+  const tableName = "urls";
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "secret",
+    database: "tbl_booklist",
+  });
+
+  connection.connect(async function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log(`Connected to MySQL database with ID ${connection.threadId}`);
+    connection.query(`SELECT * FROM ${tableName}`, async function (
+      err,
+      result,
+      fields
+    ) {
+      result.forEach((element) => {
+        tblArr.push({
+          id: element.id,
+          title: element.title,
+          author: element.author,
+        });
+      });
+    });
+  });
+
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  while (count <= tblArr.length) {
+    await tblArrPop();
+  }
+
+  async function tblArrPop() {
+    let focus = 3;
+    let element = tblArr.pop();
+    await search(element, focus);
+  }
+
+  async function search(element, f) {
+    let focus = f;
+    let query = await getQuery(element.title, element.author, focus);
+    let result = [];
+    let page = await browser.newPage({});
+    let searchURL = `https://celalibrary.ca/bibliographic-search?search_term=${query}&sort_bef_combine=search_api_relevance%20DESC&searchPref=yes&f%5B0%5D=audio_narrator_type%3A15238`;
+    await page.goto(searchURL);
+    let numResults = await page
+      .evaluate(() => {
+        let numResults = document
+          .querySelector("div.view-header > p")
+          .innerText.match(/\d*(?= items)/g)[0];
+        return parseInt(numResults);
+      })
+      .catch(() => {
+        return 0;
+      });
+    for (i = 1; i <= numResults; i++) {
+      let url = await page
+        .evaluate((i) => {
+          let node = document
+            .querySelector(
+              `div.view-content > div:nth-child(${i}) > div > div.book-title-block--cover-title > div.book-title-block--title-author > h2`
+            )
+            .innerHTML.match(/(?<=node\/)\d*/g)[0];
+          return `https://celalibrary.ca/node/${node}`;
+        }, i)
+        .catch(() => {
+          return ``;
+        });
+      result.push(url);
+    }
+    page.close();
+    console.log(
+      `search: "${decodeURI(
+        query
+      )}"\nsearchURL: ${searchURL}\nhits: ${numResults}\nresult:`,
+      result,
+      `\n `
+    );
+    updateTable(result, element, searchURL);
+    if (numResults == 1) {
+      return;
+    }
+    if (numResults > 1) {
+      if (focus == 4) return;
+      else {
+        focus++;
+        console.log(`narrowing results...`);
+        search(element, focus);
+      }
+    }
+    return;
+  }
+
+  function updateTable(result, element, searchurl) {
+    let sql = `UPDATE ${tableName} SET hits = ${result.length}, urls = '${
+      result.length > 0 ? result[0] : ""
+    }', searchurl = ${JSON.stringify(searchurl).replace(/'"'/g, "")} WHERE id=${
+      element.id
+    }`;
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+    });
+  }
+
+  async function getQuery(title, author, focus) {
+    let s1 = title.replace(/((, The$)|(, A$)|(,The$)|(,A$))/g, "");
+    if (focus < 4) {
+      s1 = keyword_extractor
+        .extract(s1, {
+          language: "english",
+          remove_digits: false,
+          return_changed_case: true,
+          remove_duplicates: true,
+        })
+        .slice(0, focus)
+        .join(" ");
+    }
+    let s2 = author.toLowerCase().replace(/,/g, "");
+    return encodeURI(`${s1} ${s2}`);
+  }
+}
